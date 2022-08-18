80b33994a5ea1c347d238f89176f4d41c2c0bb69
richforest0506 <richforest0506@gmail.com>
2022-08-18 16:54:42 +0200

    adjusted footer and form content

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -2,29 +2,24 @@ import React from 'react';
 
 const Hero = () => {
 	return (
-		<section className='bg-gray-50 h-screen flex justify-center items-center'>
+		<section className='text-white bg-gray-900'>
 			<div className='max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex'>
-				<div className='max-w-xl mx-auto text-center'>
-					<h1 className='text-3xl font-extrabold sm:text-5xl'>
-						Understand User Flow.
-						<strong className='font-extrabold text-indigo-600 sm:block'>Increase Conversion.</strong>
+				<div className='max-w-3xl mx-auto text-center'>
+					<h1 className='text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600'>
+						<span className='sm:block'>Profesjonalna opieka.</span>
+						<span className='sm:block'>Spokojny właściciel.</span>
 					</h1>
 
-					<p className='mt-4 sm:leading-relaxed sm:text-xl'>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus numquam ea!
+					<p className='max-w-xl mx-auto mt-4 sm:leading-relaxed sm:text-xl'>
+						Jesteśmy hotelem dla kotów który stawia na komfort futrzaków oraz właścicieli ponad wszystko, zapewniamy
+						spokój rodzicom pociech oraz najlepsze wakacje dla przyjezdnych.
 					</p>
 
 					<div className='flex flex-wrap justify-center gap-4 mt-8'>
 						<a
-							className='block w-full px-12 py-3 text-sm font-medium text-white bg-indigo-600 rounded shadow sm:w-auto active:bg-red-500 hover:bg-indigo-600 focus:outline-none focus:ring'
-							href='/get-started'>
-							Get Started
-						</a>
-
-						<a
-							className='block w-full px-12 py-3 text-sm font-medium text-indigo-600 rounded shadow sm:w-auto hover:text-indigo-600 active:text-red-500 focus:outline-none focus:ring'
-							href='/about'>
-							Learn More
+							className='block w-full px-12 py-3 text-sm font-medium text-white bg-indigo-600 border border-indigo-600 rounded sm:w-auto active:text-opacity-75 hover:bg-transparent hover:text-white focus:outline-none focus:ring'
+							href='#details'>
+							Dowiedz się więcej
 						</a>
 					</div>
 				</div>
